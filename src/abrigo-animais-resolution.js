934ef1d85d464671539b5d1614879c49fbbec2e5
anlzgws <annaluiza.guimaraes2@gmail.com>
2025-09-09 20:19:12 -0300

    second-commit

---
+++ src/abrigo-animais-resolution.js
@@ -0,0 +1,25 @@
+class AbrigoiAnimais {
+    encontrarPessoas(brinquedo1, brinquedo2, animais)
+}
+
+export {AbrigoiAnimais};
+
+const animaisDados = {
+    Rex: {tipo: "cão", brinquedos: ["RATO", "BOLA" ]},
+    Mimi: {tipo: "gato", brinquedos: ["BOLA", "LASER"]},
+    Fofo: {tipo: "gato", brinquedos: ["BOLA", "RATO", "LASER"]},
+    Zero: {tipo: "gato", brinquedos: ["RATO", "BOLA"]},
+    Bola: {tipo: "cão", brinquedos: ["CAIXA", "NOVELO"]}
+    Bebe: {tipo: "cão", brinquedos: ["LASER", "RATO, BOLA"]}
+
+    }
+
+
+
+    }
+
+
+
+
+    }
+}
